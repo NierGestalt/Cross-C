f47d374ed1093215f28a1418f2d01f6c8f06ae1c
Ariel Jared Arrellin <Ariel_Arrellin@yahoo.com>
2024-10-08 14:38:48 -0600

    Created using Colab

--- App.py
+++ App.py
@@ -4,8 +4,7 @@
   "metadata": {
     "colab": {
       "provenance": [],
-      "authorship_tag": "ABX9TyN5fx+2Nn0KkGqg9lB41DUz",
-      "include_colab_link": true
+      "authorship_tag": "ABX9TyN5fx+2Nn0KkGqg9lB41DUz"
     },
     "kernelspec": {
       "name": "python3",
@@ -16,16 +15,6 @@
     }
   },
   "cells": [
-    {
-      "cell_type": "markdown",
-      "metadata": {
-        "id": "view-in-github",
-        "colab_type": "text"
-      },
-      "source": [
-        "<a href=\"https://colab.research.google.com/github/NierGestalt/Cross-C/blob/master/App.py\" target=\"_parent\"><img src=\"https://colab.research.google.com/assets/colab-badge.svg\" alt=\"Open In Colab\"/></a>"
-      ]
-    },
     {
       "cell_type": "code",
       "execution_count": 1,
